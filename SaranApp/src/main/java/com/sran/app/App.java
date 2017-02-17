30f7fb88221f575a72d8ae340686bd0eacd1d45e
Saran <cogent1718@gmail.com>
2017-02-17 13:40:20 -0500

    Initial check in

---
+++ SaranApp/src/main/java/com/sran/app/App.java
@@ -0,0 +1,15 @@
+package com.sran.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	//git update
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome Saran World!" );
+    }
+}
