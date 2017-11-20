ac08f8b7cc33eadc9b42ccfd188cc9184c7ff7e3
sarantest <sarandivya@gmail.com>
2017-11-20 10:26:20 -0500

    This is first commit

---
+++ swagger-example/src/main/java/com/saran/springboot/swaggerexample/SwaggerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.saran.springboot.swaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExampleApplication {
+//This is main method
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerExampleApplication.class, args);
+	}
+}
